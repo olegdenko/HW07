71fdc741cc04da9ff01fbb9f5428a795e3e990eb
Denko <officedenko@gmail.com>
2023-06-22 16:04:24 +0300

    all

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="clean_folder",
+    description='Script to sort the folder',
     version="1.0",
     packages=find_packages(),
     entry_points={"console_scripts": ["clean-folder = clean_folder.clean:main"]},
